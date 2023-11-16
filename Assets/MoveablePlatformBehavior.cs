d2c906cac4c09a26540fb7c6ba953cae285e43c1
KOLIP-LAPTOP\GBC <sinan.kolip@georgebrown.ca>
2023-11-16 10:38:02 -0500

    Platformer movement with different direction added

--- Assets/MoveablePlatformBehavior.cs
+++ Assets/MoveablePlatformBehavior.cs
@@ -4,10 +4,18 @@ using UnityEngine;
 
 public class MoveablePlatformBehavior : MonoBehaviour
 {
+    
+    public PlatformerMovementTypes _type;
+
     [SerializeField]
     float _horizontalSpeed = 5;
     [SerializeField]
-    float _travelDistance = 5;
+    float _verticalSpeed = 5;
+
+    [SerializeField]
+    float _horizontalTravelDistance = 5;
+    [SerializeField]
+    float _verticalTravelDistance = 3;
 
     Vector3 _startPosition;
     // Start is called before the first frame update
@@ -19,7 +27,33 @@ public class MoveablePlatformBehavior : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.position = new Vector2(Mathf.PingPong(_horizontalSpeed * Time.time, _travelDistance) + _startPosition.x,
-                                        transform.position.y);
+        Move();
     }
+
+    void Move()
+    {
+        switch(_type)
+        {
+            case PlatformerMovementTypes.HORIZONTAL:
+                transform.position = new Vector2(Mathf.PingPong(_horizontalSpeed * Time.time, _horizontalTravelDistance) + _startPosition.x,
+                                        transform.position.y);
+                break;
+            case PlatformerMovementTypes.VERTICAL:
+                transform.position = new Vector2(transform.position.x,
+                                                 Mathf.PingPong(_verticalSpeed * Time.time, _verticalTravelDistance) + _startPosition.y);
+                break;
+            case PlatformerMovementTypes.DIAGONAL_RIGHT:
+                transform.position = new Vector2(Mathf.PingPong(_horizontalSpeed * Time.time, _horizontalTravelDistance) + _startPosition.x,
+                                                 Mathf.PingPong(_verticalSpeed * Time.time, _verticalTravelDistance) + _startPosition.y);
+
+                break;
+            case PlatformerMovementTypes.DIAGONAL_LEFT:
+                transform.position = new Vector2(_startPosition.x - Mathf.PingPong(_horizontalSpeed * Time.time, _horizontalTravelDistance),
+                                                  Mathf.PingPong(_verticalSpeed * Time.time, _verticalTravelDistance) + _startPosition.y);
+                break;
+            case PlatformerMovementTypes.CUSTOM:
+                break;
+        }
+    }
+
 }
